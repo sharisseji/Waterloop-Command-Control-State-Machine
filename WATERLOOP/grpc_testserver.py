a4ed389b23c5535edc9b39618ec8748b63069699
sharisseji <sharisse.wang.ji@gmail.com>
2025-02-10 14:05:18 -0500

    Cleaned up test server

--- WATERLOOP/grpc_testserver.py
+++ WATERLOOP/grpc_testserver.py
@@ -8,14 +8,6 @@ import test_pb2_grpc
 class TestService(test_pb2_grpc.TestServiceServicer):
     def SendTestMessage(self, request, context):
         if request.board_type == test_pb2.BoardType.MC:
-            # if request.HasField("mc_data_type"):
-            #     # Example response for MCDataType
-            #     return test_pb2.TestResponse(
-            #         example_message="MC data received",
-            #         motor_status="Motor is running smoothly",
-            #         motor_rpm=3200
-            #     )
-            #if request.board_type == test_pb2.MC:  # Only handle MC board type
             if request.HasField("mc_data_type"):
                 mc_type = request.mc_data_type
 
@@ -23,42 +15,42 @@ class TestService(test_pb2_grpc.TestServiceServicer):
                 if mc_type == test_pb2.BATTERY_VOLTAGE:
                     return test_pb2.TestResponse(
                         message="Battery voltage data",
-                        battery_voltage=48.5
+                        battery_voltage=48.5            # Example value
                     )
                 elif mc_type == test_pb2.BATTERY_CURRENT:
                     return test_pb2.TestResponse(
                         message="Battery current data",
-                        battery_current=120.3
+                        battery_current=120.3           # Example value
                     )
                 elif mc_type == test_pb2.SPEED:
                     return test_pb2.TestResponse(
                         message="Motor speed data",
-                        speed=3200
+                        speed=3200                      # Example value
                     )
                 elif mc_type == test_pb2.MOTOR_TEMP:
                     return test_pb2.TestResponse(
                         message="Motor temperature data",
-                        motor_temp=75.2
+                        motor_temp=75.2                 # Example value
                     )
                 elif mc_type == test_pb2.DRIVING_DIRECTION:
                     return test_pb2.TestResponse(
                         message="Driving direction data",
-                        driving_direction="Forward"
+                        driving_direction="Forward"     # Example value
                     )
                 elif mc_type == test_pb2.VOLTAGE:
                     return test_pb2.TestResponse(
                         message="Motor voltage data",
-                        voltage=400.1
+                        voltage=400.1                   # Example value
                     )
                 elif mc_type == test_pb2.CURRENT:
                     return test_pb2.TestResponse(
                         message="Motor current data",
-                        current=35.7
+                        current=35.7                    # Example value
                     )
                 elif mc_type == test_pb2.CTRL_TEMP:
                     return test_pb2.TestResponse(
                         message="Controller temperature data",
-                        ctrl_temp=60.5
+                        ctrl_temp=60.5                  # Example value
                     )
                 else:
                     return test_pb2.TestResponse(message="Unknown MC data type")
